3df22356e093ed09a6e861c386e994a16388c3b0
Mauro Werder <mauro3@runbox.com>
2021-10-26 17:30:48 +0200

    Using @testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,8 @@ using Test
 
 include("../scripts/car_travels.jl")
 
-@test update_position(0.0, 10, 1, 1, 200) == (10,1)
-@test update_position(0.0, 10, -1, 1, 200) == (-10,1)
-@test update_position(0.0, 10, -1, 1, 200) == (-10,1)
+@testset "update_position" begin
+    @test update_position(0.0, 10, 1, 1, 200) == (10,1)
+    @test update_position(0.0, 10, -1, 1, 200) == (-10,1)
+    @test update_position(0.0, 10, -1, 1, 200) == (-10,1)
+end
