10ef7f4abd8f1d116d17362537d0c531d50d69ca
Mauro Werder <mauro3@runbox.com>
2021-10-26 17:37:45 +0200

    Test floats with approx instead of ==

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,12 @@ using Test
 include("../scripts/car_travels.jl")
 
 @testset "update_position" begin
-    @test update_position(0.0, 10, 1, 1, 200) == (10,1)
-    @test update_position(0.0, 10, -1, 1, 200) == (-10,1)
-    @test update_position(0.0, 10, -1, 1, 200) == (-10,1)
+    @test update_position(0.0, 10, 1, 1, 200)[1] ≈ 10.0
+    @test update_position(0.0, 10, 1, 1, 200)[2] == 1
+
+    @test update_position(0.0, 10, -1, 1, 200)[1] ≈ -10.0
+    @test update_position(0.0, 10, -1, 1, 200)[2] == 1
+
+    @test update_position(0.0, 10, -1, 1, 200)[1] ≈ -10.0
+    @test update_position(0.0, 10, -1, 1, 200)[2] == 1
 end
